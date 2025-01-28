c219d8b1a127e1bf0b49348ff1d218512a12852a
Sadeed Bin Sadik <70870990+Sadeed-BS@users.noreply.github.com>
2025-01-29 01:32:02 +0530

    hero section completed

--- bandage/src/components/HomeHeader.jsx
+++ bandage/src/components/HomeHeader.jsx
@@ -13,7 +13,7 @@ const HomeHeader = () => {
                 <img className="w-28" src={assets.group_profiles} alt="" />
                 <p>Simply browse through our extensive list of trusted doctors, <br className="hidden sm:block" />schedule your appointment hassle-free.</p>
             </div>
-            <a href='#speciality' className="flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 duration-300" href="">Book Appointment <img className="w-3" src={assets.arrow_icon} alt="" /></a>
+            <a href='#speciality' className="flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 duration-300" >Book Appointment <img className="w-3" src={assets.arrow_icon} alt="" /></a>
         </div>
 
         <div className="md:w-1/2 relative">
